61cd0662744fcbbbb6a846f936898f51658325c1
Jo'rabek <jorabek@jora.uz>
2025-07-17 11:25:03 +0500

    fx

--- app/Filament/Resources/TicketPriorityResource.php
+++ app/Filament/Resources/TicketPriorityResource.php
@@ -3,15 +3,14 @@
 namespace App\Filament\Resources;
 
 use App\Filament\Resources\TicketPriorityResource\Pages;
-use App\Filament\Resources\TicketPriorityResource\RelationManagers;
+
 use App\Models\TicketPriority;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
+
 
 class TicketPriorityResource extends Resource
 {
@@ -83,12 +82,7 @@ class TicketPriorityResource extends Resource
             ]);
     }
 
-    public static function getRelations(): array
-    {
-        return [
-            //
-        ];
-    }
+
 
     public static function getPages(): array
     {
