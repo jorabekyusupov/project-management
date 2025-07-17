53e6b484dfad7da37a99b3f2710e68e70f9764d0
Jo'rabek <jorabek@jora.uz>
2025-07-17 10:59:58 +0500

    fx

--- app/Filament/Resources/ProjectResource/Pages/CreateProject.php
+++ app/Filament/Resources/ProjectResource/Pages/CreateProject.php
@@ -17,11 +17,12 @@ class CreateProject extends CreateRecord
 
         if ($createDefaultStatuses) {
             $defaultStatuses = [
-                ['name' => 'Backlog', 'color' => '#6B7280', 'sort_order' => 0],
-                ['name' => 'To Do', 'color' => '#F59E0B', 'sort_order' => 1],
-                ['name' => 'In Progress', 'color' => '#3B82F6', 'sort_order' => 2],
-                ['name' => 'Review', 'color' => '#8B5CF6', 'sort_order' => 3],
-                ['name' => 'Done', 'color' => '#10B981', 'sort_order' => 4]
+                ['name' => 'Черновик', 'color' => '#6B7280', 'sort_order' => 0],
+                ['name' => 'Задачи', 'color' => '#F59E0B', 'sort_order' => 1],
+                ['name' => 'В процессе', 'color' => '#3B82F6', 'sort_order' => 2],
+                ['name' => 'Готово', 'color' => '#8B5CF6', 'sort_order' => 3],
+                ['name' => 'Тестирование', 'color' => '#1127ba', 'sort_order' => 4],
+                ['name' => 'Законченный', 'color' => '#00ff19', 'sort_order' => 5]
             ];
 
             foreach ($defaultStatuses as $status) {
